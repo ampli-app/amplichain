7af24ce309b97a1af0603c785b4da5c8b27a2774
Wojciech Piotrowski <wpiotrowski00@gmail.com>
2025-03-04 18:26:22 +0100

    zmiany

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import ErrorBoundary from './components/ErrorBoundary'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root")
+if (!rootElement) throw new Error("Failed to find the root element")
+
+const root = createRoot(rootElement)
+
+root.render(
+  <StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </StrictMode>
+)
